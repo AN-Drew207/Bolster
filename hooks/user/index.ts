5ede7ad2397cdb514ab2572cd3e64c4c1a0a0ebe
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2022-10-27 23:05:51 -0400

    fixed build

--- hooks/user/index.ts
+++ hooks/user/index.ts
@@ -1,7 +1,7 @@
 import { useSession } from 'next-auth/client';
 import { UserType } from 'interfaces';
 import { GET_USER } from 'api';
-import { ProfilApiService } from 'api';
+import { ProfileApiService } from 'api';
 import { useQuery } from 'react-query';
 import { ProfileResponse } from 'api/dto/Profile.dto';
 
@@ -9,7 +9,7 @@ export const useUser = () => {
 	const [session] = useSession();
 	const user = session?.user ? (session.user as UserType) : undefined;
 	let { data: apiUser } = useQuery<UserType>([GET_USER], () =>
-		ProfilApiService.getUser()
+		ProfileApiService.getUser()
 	);
 
 	if (apiUser) {
