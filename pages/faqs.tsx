2c1e81d967f240830a8200ddc517747f97019d14
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2023-01-06 14:45:05 -0400

    fixed errors

---
+++ pages/faqs.tsx
@@ -0,0 +1,28 @@
+import {
+	OfferComponent,
+	MainSectionComponent,
+	HowWeDoItComponent,
+	TestimonialsComponent,
+	CTAComponent,
+	WhyShouldComponent,
+	LegalComponent,
+	TypeOfBolsterComponent,
+} from 'components/landing/mainSection';
+import * as React from 'react';
+
+const Home = () => {
+	return (
+		<>
+			<MainSectionComponent />
+			<OfferComponent />
+			<HowWeDoItComponent />
+			<TestimonialsComponent />
+			<CTAComponent />
+			<TypeOfBolsterComponent />
+			{/* <ArtistsAndProjectsComponent /> */}
+			<WhyShouldComponent />
+			<LegalComponent />
+		</>
+	);
+};
+export default Home;
